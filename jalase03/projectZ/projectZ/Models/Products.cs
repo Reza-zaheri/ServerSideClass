f13f0441f64d706dc9810683234a0a2c4c96d0b8
Reza Zaheri <90191604+Reza-zaheri@users.noreply.github.com>
2023-11-06 23:39:03 +0330

    jalase03

---
+++ jalase03/projectZ/projectZ/Models/Products.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projectZ.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [MaxLength]
+        public string Specification { get; set; }
+        [MaxLength]
+        public string Description { get; set; }
+        [Required]
+        [StringLength(75)]
+        public string Category { get; set; }
+        public Nullable<int> Price { get; set; }
+        [Required]
+        public bool Exist { get; set; }
+
+    }
+}
